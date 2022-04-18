6342011699b275720bbd55b51b93a013f14a8e9b
Karishma Daya <kdaya001@gmail.com>
2022-04-18 12:38:04 +1000

    update page not fonud

--- client/src/components/NotFound.tsx
+++ client/src/components/NotFound.tsx
@@ -1,5 +1,13 @@
+import './notfound.css';
+const notFound = require('../assets/not-found.gif');
+
 export const NotFound = () => {
-  return(
-    <div>Page Not Found</div>
-  )
-}
+  return (
+    <div>
+      <h1>Page Not Found</h1>
+      <div className='not-found_container'>
+        <img src={notFound} alt='....loading' />
+      </div>
+    </div>
+  );
+};
